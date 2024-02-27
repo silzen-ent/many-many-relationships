fcbab98e4c36f971ea8ba6ab55a7b19e7571e480
Alan Silva <silzen.ent@gmail.com>
2024-02-27 16:02:10 -0700

    Completed Codealong: M-2-M Relationships

--- server/seed.py
+++ server/seed.py
@@ -3,14 +3,19 @@
 
 import datetime
 from app import app
-from models import db, Employee, Meeting, Project
+from models import db, Employee, Meeting, Project, Assignment, employee_meetings
+
 
 with app.app_context():
 
     # Delete all rows in tables
+    db.session.query(employee_meetings).delete()
+    db.session.commit()
     Employee.query.delete()
     Meeting.query.delete()
     Project.query.delete()
+    Assignment.query.delete()
+
 
     # Add employees
     e1 = Employee(name="Uri Lee", hire_date=datetime.datetime(2022, 5, 17))
@@ -19,6 +24,7 @@ with app.app_context():
     e4 = Employee(name="Taylor Jai", hire_date=datetime.datetime(2015, 1, 2))
     db.session.add_all([e1, e2, e3, e4])
     db.session.commit()
+
 
     # Add meetings
     m1 = Meeting(topic="Software Engineering Weekly Update",
@@ -38,6 +44,35 @@ with app.app_context():
     db.session.add_all([p1, p2])
     db.session.commit()
 
+
     # Many-to-many relationship between employee and meeting
 
+    # Add meetings to an employee
+    e1.meetings.append(m1)
+    e1.meetings.append(m2)
+    # Add employees to a meeting
+    m2.employees.append(e2)
+    m2.employees.append(e3)
+    m2.employees.append(e4)
+    db.session.commit()
+
+
     # Many-to-many relationship between employee and project through assignment
+    a1 = Assignment(role='Project manager',
+                    start_date=datetime.datetime(2023, 5, 28),
+                    end_date=datetime.datetime(2023, 10, 30),
+                    employee=e1,
+                    project=p1)
+    a2 = Assignment(role='Flask programmer',
+                    start_date=datetime.datetime(2023, 6, 10),
+                    end_date=datetime.datetime(2023, 10, 1),
+                    employee=e2,
+                    project=p1)
+    a3 = Assignment(role='Flask programmer',
+                    start_date=datetime.datetime(2023, 11, 1),
+                    end_date=datetime.datetime(2024, 2, 1),
+                    employee=e2,
+                    project=p2)
+
+    db.session.add_all([a1, a2, a3])
+    db.session.commit()
